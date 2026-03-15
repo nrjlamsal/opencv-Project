5b0d292ba2586f13badb9c782d0ffa889c6460d1
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-15 08:38:24 -0700

    Gausian Blue

--- program.py
+++ program.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 # reading image 
 
-# img=cv.imread("niraj.jpg")
+img=cv.imread("niraj.jpg")
 
-# if img is None:
-#     print("No image is found")
-# else:
-#     print(img.shape)
-#     y,x=img.shape[:2]
-#     cv.putText(img," THis is Niraj",(x//2,y//2),cv.FONT_HERSHEY_COMPLEX,2,(255,0,0),2)
-    
-#     cv.imshow("Rotated Image ",img)
-#     cv.waitKey(0)
-#     cv.destroyAllWindows()
+if img is None:
+    print("No image is found")
+else:
+    print(img.shape)
+    y,x=img.shape[:2]
+    blurred= cv.GaussianBlur(img,(x,y),10)
+     
+    cv.imshow("Noraml Image ",img)
+    cv.imshow("Blured ",blurred)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 
@@ -20,26 +21,27 @@ import cv2 as cv
 
 # #reading videos
 
-vd=cv.VideoCapture(0)
+# vd=cv.VideoCapture(0)
 
-width=int(vd.get(cv.CAP_PROP_FRAME_WIDTH))
-height=int(vd.get(cv.CAP_PROP_FRAME_HEIGHT))
-codec=cv.VideoWriter_fourcc(*'XVID')
-recorder=cv.VideoWriter("reocrded.mp4",codec,20,(width,height))
+# width=int(vd.get(cv.CAP_PROP_FRAME_WIDTH))
+# height=int(vd.get(cv.CAP_PROP_FRAME_HEIGHT))
+# codec=cv.VideoWriter_fourcc(*'XVID')
+# recorder=cv.VideoWriter("reocrded.mp4",codec,1,(width,height))
 
-while True:
-    sucess,frame= vd.read()
-    if not sucess:
-        break
-    recorder.write(frame)
-    cv.imshow("Recording live",frame)
+# while True:
+#     sucess,frame= vd.read()
+#     if not sucess:
+#         break
+#     frame = cv.flip(frame,1) 
+#     recorder.write(frame)
+#     cv.imshow("Recording live",frame)
     
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-vd.release()
-recorder.release()
-cv.destroyAllWindows()
+# vd.release()
+# recorder.release()
+# cv.destroyAllWindows()
 
      
 
