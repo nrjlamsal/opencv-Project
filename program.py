afebfbbf0e67c82216cb39f438f3d4349705bb46
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-15 16:02:28 -0700

    ONline sagar sir face ,eye and smile detector

--- program.py
+++ program.py
@@ -1,85 +1,55 @@
 import cv2 as cv
 
-img = cv.imread("niraj.jpg")
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv.CascadeClassifier("haarcascade_eye.xml")
+smile_cascade = cv.CascadeClassifier("haarcascade_smile.xml")
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cap = cv.VideoCapture(0)
 
-_, thresh = cv.threshold(gray,130,255,cv.THRESH_BINARY)
+while True:
 
-contours, hierarchy = cv.findContours(
-        thresh,
-        cv.RETR_EXTERNAL,
-        cv.CHAIN_APPROX_SIMPLE
-)
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-# cv.drawContours(img, contours, -1, (134,180,255), 2)
-for cnt in contours:
+    # flip camera for mirror view
+    frame = cv.flip(frame, 1)
 
-    perimeter = cv.arcLength(cnt, True)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    approx = cv.approxPolyDP(cnt, 0.02*perimeter, True)
-    shape=""
-    sides = len(approx)
-    if sides == 3:
-        shape = "Triangle"
-    elif sides == 4:
-        shape = "Rectangle"
-    elif sides > 4:
-        shape = "Circle"
-    cv.drawContours(img,[approx],0,(0,255,0),2)
-    x = approx.ravel()[0]
-    y = approx.ravel()[1]-10
-    cv.putText(img, shape, (x,y),
-               cv.FONT_HERSHEY_COMPLEX,
-               0.6,
-               (255,0,0),
-               2)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
+    for (x, y, w, h) in faces:
 
-cv.imshow("Contours", img)
-cv.waitKey(0)
+        cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        # Detect eyes
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 10)
+        if len(eyes) > 0:
+            cv.putText(frame, "Eyes Detected",
+                       (x, y-30),
+                       cv.FONT_HERSHEY_COMPLEX,
+                       0.7,
+                       (0,255,0),
+                       2)
+
+        # Detect smile
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 20)
+        if len(smiles) > 0:
+            cv.putText(frame, "Smile Detected",
+                       (x, y-10),
+                       cv.FONT_HERSHEY_COMPLEX,
+                       0.7,
+                       (0,255,0),
+                       2)
+
+    cv.imshow("WebCam For Face Detection", frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv.destroyAllWindows()
-
-
-
-
-# #reading videos
-
-# vd=cv.VideoCapture(0)
-
-# width=int(vd.get(cv.CAP_PROP_FRAME_WIDTH))
-# height=int(vd.get(cv.CAP_PROP_FRAME_HEIGHT))
-# codec=cv.VideoWriter_fourcc(*'XVID')
-# recorder=cv.VideoWriter("reocrded.mp4",codec,1,(width,height))
-
-# while True:
-#     sucess,frame= vd.read()
-#     if not sucess:
-#         break
-
-#     frame = cv.flip(frame,1) 
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     _, thresh = cv.threshold(gray,127,255,cv.THRESH_BINARY)
-#     contours, hierarchy = cv.findContours(
-#         thresh,
-#         cv.RETR_EXTERNAL,
-#         cv.CHAIN_APPROX_SIMPLE
-#         )
-#     cv.drawContours(frame, contours, -1, (0,0,0), 2)
-
-#     # recorder.write(frame)
-#     cv.imshow("Recording live",frame)
-    
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# vd.release()
-# # recorder.release()
-# cv.destroyAllWindows()
-
-     
-
-
-
-
- 
