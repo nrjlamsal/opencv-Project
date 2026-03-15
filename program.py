4cce67fc8dbf5691cb1d961f6955ab1eb3e79de9
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-15 07:04:01 -0700

    Simple video recoding

--- program.py
+++ program.py
@@ -1,18 +1,18 @@
 import cv2 as cv
 # reading image 
 
-img=cv.imread("niraj.jpg")
+# img=cv.imread("niraj.jpg")
 
-if img is None:
-    print("No image is found")
-else:
-    print(img.shape)
-    y,x=img.shape[:2]
-    cv.putText(img," THis is Niraj",(x//2,y//2),cv.FONT_HERSHEY_COMPLEX,2,(255,0,0),2)
+# if img is None:
+#     print("No image is found")
+# else:
+#     print(img.shape)
+#     y,x=img.shape[:2]
+#     cv.putText(img," THis is Niraj",(x//2,y//2),cv.FONT_HERSHEY_COMPLEX,2,(255,0,0),2)
     
-    cv.imshow("Rotated Image ",img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+#     cv.imshow("Rotated Image ",img)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
 
 
 
@@ -20,15 +20,21 @@ else:
 
 # #reading videos
 
-# capture=cv.VideoCapture("video.mp4")
+vd=cv.VideoCapture(0)
 
-# while True:
-#     isTrue ,frame= capture.read()
-#     cv.imshow("Video",frame)
-#heloo
+while True:
+   check,frame=vd.read()
+   if  not check :
+      print(" Could not read frames")
+      break
+    
+   cv.imshow("Webcam",frame)
 
-#     if cv.waitKey(20) & 0xFF == ord('d'):
-#         break
+   if cv.waitKey(1) & 0xFF==ord('q'):
+      print("qutting")
+      break
+   
+vd.release()
+cv.destroyAllWindows()
 
-# capture.release()
-# cv.destroyALLWindows()
+ 
