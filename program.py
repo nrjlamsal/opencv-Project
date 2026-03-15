bdc698f6867bf53c55e802dd91ab164dfcbb209f
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-15 14:23:55 -0700

    boundary on my face)

--- program.py
+++ program.py
@@ -1,57 +1,76 @@
-import cv2
-import numpy as np
+import cv2 as cv
 
-# Create two blank black images
-img1 = np.zeros((300, 300), dtype="uint8")
-img2 = np.zeros((300, 300), dtype="uint8")
+# img = cv.imread("niraj.jpg")
 
-# Draw a white circle on the first image
-cv2.circle(img1, (150, 150), 100, 255, -1)
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# Draw a white rectangle on the second image
-cv2.rectangle(img2, (100, 100), (250, 250), 255, -1)
+# _, thresh = cv.threshold(gray,130,255,cv.THRESH_BINARY)
 
-# Perform bitwise operations
-bitwise_and = cv2.bitwise_and(img1, img2)
-bitwise_or = cv2.bitwise_or(img1, img2)
-bitwise_not = cv2.bitwise_not(img1)
+# contours, hierarchy = cv.findContours(
+#         thresh,
+#         cv.RETR_EXTERNAL,
+#         cv.CHAIN_APPROX_SIMPLE
+# )
 
-# Display the results
-cv2.imshow("Circle", img1)
-cv2.imshow("Rectangle", img2)
-cv2.imshow("AND", bitwise_and)
-cv2.imshow("OR", bitwise_or)
-cv2.imshow("NOT", bitwise_not)
+# cv.drawContours(img, contours, -1, (134,180,255), 2)
+# for cnt in contours:
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#     perimeter = cv.arcLength(cnt, True)
 
+#     approx = cv.approxPolyDP(cnt, 0.02*perimeter, True)
+#     shape=""
+#     sides = len(approx)
+#     if sides == 3:
+#         shape = "Triangle"
+#     elif sides == 4:
+#         shape = "Rectangle"
+#     elif sides > 4:
+#         shape = "Circle"
+#     cv.drawContours(img,[approx],0,(0,255,0),2)
+#     x = approx.ravel()[0]
+#     y = approx.ravel()[1]-10
+#     cv.putText(img,shape,(x,y),cv.FONT_HERSHEY_COMPLEX,)
 
-
-
-# #reading videos
-
-# vd=cv.VideoCapture(0)
-
-# width=int(vd.get(cv.CAP_PROP_FRAME_WIDTH))
-# height=int(vd.get(cv.CAP_PROP_FRAME_HEIGHT))
-# codec=cv.VideoWriter_fourcc(*'XVID')
-# recorder=cv.VideoWriter("reocrded.mp4",codec,1,(width,height))
-
-# while True:
-#     sucess,frame= vd.read()
-#     if not sucess:
-#         break
-#     frame = cv.flip(frame,1) 
-#     recorder.write(frame)
-#     cv.imshow("Recording live",frame)
-    
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# vd.release()
-# recorder.release()
+# cv.imshow("Contours", img)
+# cv.waitKey(0)
 # cv.destroyAllWindows()
+
+
+
+
+#reading videos
+
+vd=cv.VideoCapture(0)
+
+width=int(vd.get(cv.CAP_PROP_FRAME_WIDTH))
+height=int(vd.get(cv.CAP_PROP_FRAME_HEIGHT))
+codec=cv.VideoWriter_fourcc(*'XVID')
+recorder=cv.VideoWriter("reocrded.mp4",codec,1,(width,height))
+
+while True:
+    sucess,frame= vd.read()
+    if not sucess:
+        break
+
+    frame = cv.flip(frame,1) 
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    _, thresh = cv.threshold(gray,127,255,cv.THRESH_BINARY)
+    contours, hierarchy = cv.findContours(
+        thresh,
+        cv.RETR_EXTERNAL,
+        cv.CHAIN_APPROX_SIMPLE
+        )
+    cv.drawContours(frame, contours, -1, (0,255,0), 2)
+
+    # recorder.write(frame)
+    cv.imshow("Recording live",frame)
+    
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vd.release()
+# recorder.release()
+cv.destroyAllWindows()
 
      
 
