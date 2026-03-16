c9f106ae5cee8d2c555e96d92c0a7cfbd19b70e1
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-16 12:17:21 -0700

    Detets iris and shows it

--- prog.py
+++ prog.py
@@ -1,31 +1,40 @@
 import cv2 as cv
 import mediapipe as mp
 
-# Load FaceMesh
-# refine_landmarks=True gives us the IRIS points (needed for eye tracking)
-face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+mp_face_mesh = mp.solutions.face_mesh
+
+face_mesh = mp_face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True
+)
 
 cap = cv.VideoCapture(0)
 
 while True:
+
     ret, frame = cap.read()
-    frame = cv.flip(frame, 1)
+    frame = cv.flip(frame,1)
+    frame_height,frame_width = frame.shape[:2]
 
-    # FaceMesh needs RGB, but OpenCV gives BGR — so we convert
     rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    results = face_mesh.process(rgb)
 
-    # Process the frame — this gives us all 468 landmarks
-    result = face_mesh.process(rgb)
+    if results.multi_face_landmarks:
+         # Loop over faces
+        face1= results.multi_face_landmarks[0]
 
-    # Check if any face was detected
-    if result.multi_face_landmarks:
-        print(result.multi_face_landmarks)
-        print("Face detected!")
+        liris=face1.landmark[468]
+        x = int(liris.x * frame_width)
+        y = int(liris.y * frame_height)     
+        cv.circle(frame, (x, y), 1, (0,255,0), -1)
 
-    else:
-        print("No face detected")
+        riris=face1.landmark[473]
+        x = int(riris.x * frame_width)
+        y = int(riris.y * frame_height)     
+        cv.circle(frame, (x, y), 1, (0,255,0), -1)
 
-    cv.imshow("FaceMesh", frame)
+    cv.imshow("Iris Tracking", frame)
+
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
